2ec6171b19f78a3d2cf932ab3a528493110cdb65
ssjpooh <tkdwnslpooh@gmail.com>
2023-10-30 18:07:37 +0900

    room list 예제

--- controller/authApi/roomApi/roomApi.go
+++ controller/authApi/roomApi/roomApi.go
@@ -2,32 +2,37 @@ package roomapi
 
 import (
 	"net/http"
-	authHandler "restApi/util/auth"
 
 	rooms "restApi/model/rooms"
 	dbHandler "restApi/util/db"
 
 	logger "restApi/util/log"
 
+	authHandler "restApi/util/auth"
+
 	"github.com/gin-gonic/gin"
 )
 
 func getRoomList(context *gin.Context) []rooms.Room {
+
+	logger.Logger(logger.GetFuncNm(), "1")
 	var roomList []rooms.Room
 
-	query := "SELECT ROOM_NO, TITLE FROM ROOM_TBL "
-
+	query := "SELECT room_no, title FROM CONF_TBL "
+	logger.Logger(logger.GetFuncNm(), "2")
 	err := dbHandler.Db.Select(&roomList, query)
 	if err != nil {
+		logger.Logger(logger.GetFuncNm(), "3")
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "not Found"})
 		logger.Logger(logger.GetFuncNm(), "select error : ", err.Error())
 	}
-
+	logger.Logger(logger.GetFuncNm(), "4")
 	return roomList
 
 }
 func RoomApiHandler(v1 *gin.RouterGroup) {
 
+	logger.Logger(logger.GetFuncNm(), "start")
 	// @Summary get room List
 	// @Description Get room List
 	// @Tags rooms
@@ -40,4 +45,10 @@ func RoomApiHandler(v1 *gin.RouterGroup) {
 		roomList := getRoomList(context)
 		context.IndentedJSON(http.StatusOK, roomList)
 	})
+
+	v1.GET("/roomtest", func(context *gin.Context) {
+		roomList := getRoomList(context)
+		context.IndentedJSON(http.StatusOK, roomList)
+	})
+
 }
